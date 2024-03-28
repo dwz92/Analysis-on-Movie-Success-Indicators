47b5a48a4440a5efb67c21567e54c53092367787
tengqi <75350626+dwz92@users.noreply.github.com>
2024-03-28 06:07:14 -0400

    Scrapped data, stored to raw data

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -1,26 +1,41 @@
 #### Preamble ####
-# Purpose: Downloads and saves the data from [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Scrapes and saves the data from https://www.boxofficemojo.com/
+# Author: Qi Er (Emma) Teng
+# Date: 28 March 2024
+# Contact: e.teng@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Note: Datasets from IMDb are downloaded manually at https://developer.imdb.com/non-commercial-datasets/
+# Note: All web scrapping is done with permission from https://www.boxofficemojo.com/robots.txt
 
 
 #### Workspace setup ####
-library(opendatatoronto)
 library(tidyverse)
-# [...UPDATE THIS...]
+library(rvest)
 
-#### Download data ####
-# [...ADD CODE HERE TO DOWNLOAD...]
+#### Function for Scrapping ####
+scrape_tab <- function(html_list, yrlist){
+  
+  for (i in c(1:length(html_list))) {
+    web_page <- read_html(html_list[i])
+    
+    table_yr <- web_page |>
+      html_elements(xpath = "//*[contains(@class, 'a-section imdb-scroll-table-inner')]") |>
+      html_table()
+    
+    pathstr=paste("data/raw_data/", toString(yrlist[i]), ".csv", sep = "")
+    
+    write_csv(table_yr[[1]], pathstr)
+  }
+}
 
 
+#### Get HTML ####
+htmls_yr <- c("https://www.boxofficemojo.com/year/2019/?sort=grossToDate&ref_=bo_yld__resort#table",
+              "https://www.boxofficemojo.com/year/2020/?sort=grossToDate&ref_=bo_yld__resort#table",
+              "https://www.boxofficemojo.com/year/2021/?sort=grossToDate&ref_=bo_yld__resort#table",
+              "https://www.boxofficemojo.com/year/2022/?sort=grossToDate&ref_=bo_yld__resort#table")
+
+yr=c(2019, 2020, 2021, 2022)
 
 #### Save data ####
-# [...UPDATE THIS...]
-# change the_raw_data to whatever name you assigned when you downloaded it.
-write_csv(the_raw_data, "inputs/data/raw_data.csv") 
-
-         
+scrape_tab(htmls_yr, yr)
