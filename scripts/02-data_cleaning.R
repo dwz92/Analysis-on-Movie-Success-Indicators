fdc7729db603f041621ada5990923c18da313e29
tengqi <75350626+dwz92@users.noreply.github.com>
2024-03-30 20:25:45 -0400

    scripts 01, 02 done

--- scripts/02-data_cleaning.R
+++ scripts/02-data_cleaning.R
@@ -1,44 +1,76 @@
 #### Preamble ####
-# Purpose: Cleans the raw plane data recorded by two observers..... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 6 April 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Cleans the raw movie data and join with IMDb data
+# Author: Qi Er (Emma) Teng
+# Date: 28 March 2024
+# Contact: e.teng@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Note: Datasets from IMDb are downloaded manually at https://developer.imdb.com/non-commercial-datasets/
+# Note: All web scrapping is done with permission from https://www.boxofficemojo.com/robots.txt
 
 #### Workspace setup ####
 library(tidyverse)
+library(dplyr)
+library(here)
+library(janitor)
+library(arrow)
 
-#### Clean data ####
-raw_data <- read_csv("inputs/data/plane_data.csv")
+#### Join movie by yrs ####
+mov2019 <- read.csv(here::here("data/raw_data/2019.csv"))
+mov2020 <- read.csv(here::here("data/raw_data/2020.csv"))
+mov2021 <- read.csv(here::here("data/raw_data/2021.csv"))
+mov2022 <- read.csv(here::here("data/raw_data/2022.csv"))
 
-cleaned_data <-
-  raw_data |>
-  janitor::clean_names() |>
-  select(wing_width_mm, wing_length_mm, flying_time_sec_first_timer) |>
-  filter(wing_width_mm != "caw") |>
-  mutate(
-    flying_time_sec_first_timer = if_else(flying_time_sec_first_timer == "1,35",
-                                   "1.35",
-                                   flying_time_sec_first_timer)
-  ) |>
-  mutate(wing_width_mm = if_else(wing_width_mm == "490",
-                                 "49",
-                                 wing_width_mm)) |>
-  mutate(wing_width_mm = if_else(wing_width_mm == "6",
-                                 "60",
-                                 wing_width_mm)) |>
-  mutate(
-    wing_width_mm = as.numeric(wing_width_mm),
-    wing_length_mm = as.numeric(wing_length_mm),
-    flying_time_sec_first_timer = as.numeric(flying_time_sec_first_timer)
-  ) |>
-  rename(flying_time = flying_time_sec_first_timer,
-         width = wing_width_mm,
-         length = wing_length_mm
-         ) |> 
-  tidyr::drop_na()
+mov2019 <- mov2019 |>
+  mutate("release_year" = "2019") |>
+  select(Release, Theaters, Total.Gross, Release.Date, release_year)
 
-#### Save data ####
-write_csv(cleaned_data, "outputs/data/analysis_data.csv")
+mov2020 <- mov2020 |>
+  mutate("release_year" = "2020") |>
+  select(Release, Theaters, Total.Gross, Release.Date, release_year)
+
+mov2021 <- mov2021 |>
+  mutate("release_year" = "2021") |>
+  select(Release, Theaters, Total.Gross, Release.Date, release_year)
+
+mov2022 <- mov2022 |>
+  mutate("release_year" = "2022") |>
+  select(Release, Theaters, Total.Gross, Release.Date, release_year)
+
+mov1922 <- bind_rows(mov2019, mov2020, mov2021, mov2022)
+
+mov1922 <- mov1922 |>
+  clean_names() |>
+  mutate(theaters = ifelse(theaters == "-", 0, theaters)) |>
+  filter(release_date != "-")
+
+write_parquet(x = mov1922,
+              sink = "data/analysis_data/mov1922.parquet")
+
+
+#### Join title akas with mov1922 on title = release ####
+titlebasic <- read_tsv(here::here("data/raw_data/title.basics.tsv"))
+titlerate <- read_tsv(here::here("data/raw_data/title.ratings.tsv"))
+
+#Save sample of raw data
+write_parquet(titlebasic[1:10,],
+          "data/analysis_data/titlebasicraw.parquet")
+write_parquet(titlerate[1:10,],
+              "data/analysis_data/titlerateraw.parquet")
+
+titlecomb <- titlebasic |>
+  select(tconst, originalTitle, startYear, genres, titleType) |>
+  filter(originalTitle %in% mov1922$release, titleType == "movie") |>
+  inner_join(titlerate, by = c("tconst" = "tconst"))
+
+mov_s2 <- titlecomb |>
+  rename(
+    "release" = originalTitle,
+    "release_year" = startYear
+  )
+
+mov_s2 <- mov_s2 |>
+  inner_join(mov1922, by = c("release", "release_year"))
+  
+
+write_parquet(x = mov_s2,
+             sink = "data/analysis_data/movcomb.parquet")
--- scripts/04-model.R
+++ scripts/04-model.R
@@ -1,11 +1,9 @@
 #### Preamble ####
 # Purpose: Models... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Author: Qi Er (Emma) Teng
+# Date: 28 March 2024
+# Contact: e.teng@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
 
 
 #### Workspace setup ####
@@ -13,7 +11,9 @@ library(tidyverse)
 library(rstanarm)
 
 #### Read data ####
-analysis_data <- read_csv("data/analysis_data/analysis_data.csv")
+analysis_data <- read_parquet("data/analysis_data/movcomb.csv")
+
+analysis_data
 
 ### Model data ####
 first_model <-
